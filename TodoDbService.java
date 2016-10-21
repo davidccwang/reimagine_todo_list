296f964babee0d6dad796b8e699b0da3d860e835
anand8642 <anand2@gmail.com>
2016-10-21 14:26:56 -0700

    Updated TodoDbService.java

--- TodoDbService.java
+++ TodoDbService.java
@@ -1,17 +1,32 @@
 package com.Cloud4;
 
+import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import org.mindrot.jbcrypt.BCrypt;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.UpdateOperations;
 
+import java.util.List;
+
 /**
- * Created by Anand on 10/18/2016.
+ * Created by Anand on 10/21/2016.
  */
 public class TodoDbService {
+
     MongoClient todoClient = new MongoClient("localhost", 27017);
     Datastore todoDatastore = new Morphia().createDatastore(todoClient, "todos");
+
+    public List<Todo> getTodos (String username, String password) {
+        System.out.println("Username is: " + username + " Password is: " + password);
+        String hashedPassword = BCrypt.hashpw(password,BCrypt.gensalt());
+        System.out.println("Username is: " + username + " Hashed Password is: " + hashedPassword);
+        DB db = todoDatastore.getDB();
+        //db.(username,hashedPassword.toCharArray());
+        List<Todo> todoList = todoDatastore.createQuery(Todo.class).asList();
+        return todoList;
+    }
 
     public Todo getTodo (Integer id) {
         System.out.println("aaaaaaaaaaaaaa");
@@ -24,10 +39,16 @@ public class TodoDbService {
     }
 
     public String createTodo (Todo todo) {
-        System.out.println("bbbbbbbbbbbbb");
-        System.out.println(todo.getId() + todo.getTodoItem());
-        todoDatastore.save(todo);
-        return "Todo created";
+        System.out.println("Entering createTodo");
+        Integer id = todo.getId();
+        System.out.println("Id: " + id + " TodoTask: " + todo.getTodoItem());
+        if (this.getTodo(id) != null) {
+            System.out.println("Id: " + id + " TodoTask: " + todo.getTodoItem() + " Already present");
+            return("Id: " + id + " TodoTask: " + todo.getTodoItem() + " Already present");
+        } else {
+            todoDatastore.save(todo);
+            return "Todo created";
+        }
     }
 
     public String updateTodo (Todo todo) {
@@ -47,4 +68,3 @@ public class TodoDbService {
         System.out.println("Deleted " + todo.getId());
     }
 }
-
